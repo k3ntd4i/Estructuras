7a27f6055829b344ac0c784fc4c7505c558b6e3f
k3ntd4i <ackar_quentdie48@outlook.com>
2024-07-08 21:19:05 -0500

    Cuerpo de la clase ArbolBinario para la implementacion de la estructura Arbol

---
+++ arbol.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+
+template<typename T>
+class ArbolBinario
+{
+    T elemento{};
+    ArbolBinario<T> *hijo_izquierdo{ nullptr };
+    ArbolBinario<T> *hijo_derecho{ nullptr };
+
+public:
+    ArbolBinario() = default;
+
+    bool is_empty()
+    {
+
+    }
+
+    T root()
+    {
+
+    }
+
+    void make_tree(T elemento_raiz, ArbolBinario<T> arbol_izquierdo, ArbolBinario<T> arbol_derecho)
+    {
+
+    }
+
+    ArbolBinario<T> remove_left_subtree()
+    {
+
+    }
+
+    ArbolBinario<T> remove_right_subtree()
+    {
+        
+    }
+
+    void pre_order()
+    {
+        
+    }
+    
+    void in_order()
+    {
+
+    }
+
+    void post_order()
+    {
+
+    }
+
+    void level_order()
+    {
+
+    }
+};
+
+int main()
+{
+    ArbolBinario<int> arbol{};
+
+    return 0;
+}
