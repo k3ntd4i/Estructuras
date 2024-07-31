02e37c21d42caae261715350d1dc2e2e4c19c7fb
k3ntd4i <ackar_quentdie48@outlook.com>
2024-07-31 16:29:11 -0500

    Añadiendo funcionalidad a metodos

--- include/arbol_bs.hpp
+++ include/arbol_bs.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "arbol_binario.hpp"
+#include <stdexcept>
 
 template<typename T>
 class ArbolBusquedaBinaria
@@ -11,20 +12,25 @@ class ArbolBusquedaBinaria
 public:
     ArbolBusquedaBinaria() = default;
 
+    ArbolBusquedaBinaria(const T &elemento)
+        : arbol{ elemento }, cantidad_nodos{ 1 }
+    {
+    }
+
     bool is_empty()
     {
-        // por hacer
+        return (this->cantidad_nodos == 0);
     }
 
     int size()
     {
-        // por hacer
+        return this->cantidad_nodos;
     }
 
     // Profundidad maxima: cantidad total de niveles (contando desde 1)
     int height()
     {
-        return (arbol.height() - 1);
+        return (this->cantidad_nodos == 0) ? 0 : arbol.height();
     }
 
     bool contains(const T &elemento)
@@ -45,16 +51,33 @@ public:
     void make_empty()
     {
         arbol.make_tree(T{}, nullptr, nullptr);
+        cantidad_nodos = 0;
     }
 
     T find_min()
     {
-        // por hacer
+        verificar_contenido();
+
+        ArbolBinario<T> *nodo_actual{ &arbol };
+        while (nodo_actual->get_left_child() != nullptr)
+        {
+            nodo_actual = nodo_actual->get_left_child();
+        }
+
+        return nodo_actual->get_element();
     }
 
     T find_max()
     {
-        // por hacer
+        verificar_contenido();
+
+        ArbolBinario<T> *nodo_actual{ &arbol };
+        while (nodo_actual->get_right_child() != nullptr)
+        {
+            nodo_actual = nodo_actual->get_right_child();
+        }
+
+        return nodo_actual->get_element();
     }
 
     void pre_order()
@@ -75,5 +98,14 @@ public:
     void level_order()
     {
         arbol.level_order();
+    }
+
+private:
+    void verificar_contenido()
+    {
+        if (this->cantidad_nodos == 0)
+        {
+            throw std::out_of_range("El arbol esta vacio.");
+        }
     }
 };
