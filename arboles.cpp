45f33167dd10ca1ab19ecbd9d76a6b33a680eae4
k3ntd4i <ackar_quentdie48@outlook.com>
2024-07-31 16:31:10 -0500

    Primeras pruebas ABB

--- arboles.cpp
+++ arboles.cpp
@@ -1,9 +1,31 @@
 #include "include/arbol_binario.hpp"
+#include "include/arbol_bs.hpp"
 #include <iostream>
 
 int main()
 {
     ArbolBinario<int> arbol{};
+
+    ArbolBusquedaBinaria<int> bst_1{};
+    ArbolBusquedaBinaria<int> bst_2{ 4 };
+
+    std::cout << "Size: " << bst_1.size() << std::boolalpha << ", vacio: " << bst_1.is_empty();
+    std::cout << "\nAltura: " << bst_1.height() << '\n';
+
+    std::cout << "\nSize: " << bst_2.size() << std::boolalpha << ", vacio: " << bst_2.is_empty();
+    std::cout << "\nAltura: " << bst_2.height() << '\n';
+
+    std::cout << '\n';
+
+    // std::cout << bst_1.find_min();
+    // std::cout << bst_1.find_max();
+    std::cout << "[+] Minimo: " << bst_2.find_min() << '\n';
+    std::cout << "[+] Maximo: " << bst_2.find_max() << '\n';
+
+    bst_2.make_empty();
+
+    std::cout << "\nSize: " << bst_2.size() << std::boolalpha << ", vacio: " << bst_2.is_empty();
+    std::cout << "\nAltura: " << bst_2.height() << '\n';
 
     return 0;
 }
