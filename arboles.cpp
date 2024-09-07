ed0444ed2d96ac295fdb4c18bc5a173f0e0bd8af
k3ntd4i <ackar_quentdie48@outlook.com>
2024-09-06 22:55:02 -0500

    Pruebas estructura Max-Heap

--- arboles.cpp
+++ arboles.cpp
@@ -1,11 +1,31 @@
 #include "include/arbol_binario.hpp"
 #include "include/arbol_busqueda_binaria.hpp"
+#include "include/maximo_heap.hpp"
 #include <iostream>
 
 int main()
 {
     ArbolBinario<int> arbol{};
     ArbolBusquedaBinaria<int> bst{};
+    MaximoHeap<int> heap{ 15 };
+
+    for (int i{0}; i < 10; ++i)
+    {
+        std::cout << i << ' ';
+        heap.put(i);
+    }
+
+    heap.put(2);
+    heap.put(9);
+    heap.put(9);
+    heap.put(0);
+
+    std::cout << '\n';
+
+    for (int i{0}; i < 14; ++i)
+    {
+        std::cout << heap.extract_max() << ' ';
+    }
 
     return 0;
 }
