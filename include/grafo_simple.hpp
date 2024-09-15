99cb2545508e71d248b7506bad1ecfe4f7b5c2b1
k3ntd4i <ackar_quentdie48@outlook.com>
2024-09-14 23:13:42 -0500

    Avance general
    - Se agrega la estructura 'Node' para poder trackear el grado de cada nodo
    - Se reemplaza el arreglo dinamico 'nodos' por un arreglo de punteros a la estructura 'Node'
    - Se cambia el nombre del arreglo dinamico 'nodos' por 'lista_nodos'
    - Se agrega el atributo 'capacidad' para almacenar la cantidad maxima de nodos
    - Se actualiza el destructor para tratar con la estructura 'Node' adecuadamente
    - Se agregan verificadores de indicies
    - Se agrega verificador para comprobar la existencia de un nodo dado
    - Se cambian los nombres de los metodos 'remove_element' y 'set_element' por 'remove_node' y 'set_node' respectivamente
    - Se reemplaza el metodo 'remove_edge' y 'add_edge' por 'update_edge'
    	- Este puede enlazar y desenlazar nodos a traves de un booleano
    - Se finalizan los siguientes metodos:
    	- remove_node
    	- set_node
    	- contains_node
    	- get_element
    	- get_degree
    - Se agrega el metodo 'contains_edge' para comprobar la existencia de una arista entre dos nodos especificos
    - Se agrega el metodo 'print_matrix' para visualizar la matriz de adyacencia

--- include/grafo_simple.hpp
+++ include/grafo_simple.hpp
@@ -1,25 +1,43 @@
 #pragma once
 
 #include <iostream>
+#include <stdexcept>
+#include <string>
 
 template <typename T>
 class GrafoSimple
 {
-    T *nodos{};
+    struct Node
+    {
+        T elemento{};
+        int grado{};
+
+        Node() = default;
+        Node(const T &nuevo_elemento) : elemento{ nuevo_elemento }, grado{ 0 } {}
+    };
+
+    Node **lista_nodos{};
     bool *matriz_adyacencia{};
     int cantidad_nodos{};
+    int capacidad{};
 
 public:
     GrafoSimple(int numero_nodos)
-        : nodos{ new T[numero_nodos]{} }
+        : lista_nodos{ new Node*[numero_nodos]{} }
         , matriz_adyacencia{ new bool[numero_nodos * numero_nodos]{} }
         , cantidad_nodos{ 0 }
+        , capacidad{ numero_nodos }
     {
     }
 
     ~GrafoSimple()
     {
-        delete[] this->nodos;
+        for (int i{0}; i < this->capacidad; ++i)
+        {
+            delete this->lista_nodos[i];
+        }
+
+        delete[] this->lista_nodos;
         delete[] this->matriz_adyacencia;
     }
 
@@ -33,49 +51,151 @@ public:
         return this->cantidad_nodos;
     }
 
-    T remove_element(int indice_nodo)
+    T remove_node(int indice_nodo)
     {
+        verificar_indice(indice_nodo);
+        verificar_existencia(indice_nodo);
 
+        T nodo_removido{ this->lista_nodos[indice_nodo]->elemento };
+
+        delete this->lista_nodos[indice_nodo];
+        this->lista_nodos[indice_nodo] = nullptr;
+
+        for (int i{0}; i < this->capacidad; ++i)
+        {
+            if (this->matriz_adyacencia[(this->capacidad * i) + indice_nodo])
+            {
+                this->matriz_adyacencia[(this->capacidad * i) + indice_nodo] = false;
+                this->matriz_adyacencia[(this->capacidad * indice_nodo) + i] = false;
+
+                --this->lista_nodos[i]->grado;
+            }
+        }
+
+        --this->cantidad_nodos;
+        return nodo_removido;
     }
 
-    void set_element(int indice_nodo, const T &elemento)
+    void set_node(int indice_nodo, const T &elemento)
     {
+        verificar_indice(indice_nodo);
+        Node *nuevo_nodo{ new Node{elemento} };
 
+        if (this->lista_nodos[indice_nodo] != nullptr)
+        {
+            delete this->lista_nodos[indice_nodo];
+
+            this->lista_nodos[indice_nodo] = nuevo_nodo;
+            return;
+        }
+
+        this->lista_nodos[indice_nodo] = nuevo_nodo;
+        ++this->cantidad_nodos;
     }
 
-    void remove_edge(int indice_nodo_1, int indice_nodo_2)
+    void update_edge(int indice_nodo_1, int indice_nodo_2, bool enlazar)
     {
+        verificar_indice(indice_nodo_1, indice_nodo_2);
+        verificar_existencia(indice_nodo_1);
+        verificar_existencia(indice_nodo_2);
 
-    }
+        this->matriz_adyacencia[(this->capacidad * indice_nodo_1) + indice_nodo_2] = enlazar;
+        this->matriz_adyacencia[(this->capacidad * indice_nodo_2) + indice_nodo_1] = enlazar;
 
-    void add_edge(int indice_nodo_1, int indice_nodo_2)
-    {
-
+        if (enlazar)
+        {
+            ++this->lista_nodos[indice_nodo_1]->grado;
+            ++this->lista_nodos[indice_nodo_2]->grado;
+        }
+        else
+        {
+            --this->lista_nodos[indice_nodo_1]->grado;
+            --this->lista_nodos[indice_nodo_2]->grado;
+        }
     }
 
     bool contains_node(int indice_nodo)
     {
+        verificar_indice(indice_nodo);
+        return (this->lista_nodos[indice_nodo] == nullptr) ? false : true;
+    }
 
+    bool contains_edge(int indice_nodo_1, int indice_nodo_2)
+    {
+        verificar_indice(indice_nodo_1, indice_nodo_2);
+        verificar_existencia(indice_nodo_1);
+        verificar_existencia(indice_nodo_2);
+
+        return (this->matriz_adyacencia[(this->capacidad * indice_nodo_1) + indice_nodo_2]);
     }
 
     T get_element(int indice_nodo)
     {
-        return this->nodos[indice_nodo];
+        verificar_indice(indice_nodo);
+        verificar_existencia(indice_nodo);
+
+        return this->lista_nodos[indice_nodo]->elemento;
     }
 
     int get_degree(int indice_nodo)
     {
+        verificar_indice(indice_nodo);
+        verificar_existencia(indice_nodo);
 
+        return this->lista_nodos[indice_nodo]->grado;
     }
 
     void clear()
     {
-
+        // por hacer
     }
 
     // El usuario es el responsable de liberar la memoria
     T *get_adjacent_nodes(int indice_nodo)
     {
+        // por hacer
+    }
 
+    void print_matrix()
+    {
+        for (int i{0}; i < this->capacidad; ++i)
+        {
+            for (int j{0}; j < this->capacidad; ++j)
+            {
+                std::cout << this->matriz_adyacencia[(i * this->capacidad) + j] << ' ';
+            }
+            std::cout << '\n';
+        }
+    }
+
+    // hacer recorridos
+
+private:
+    void verificar_indice(int posicion)
+    {
+        if (posicion < 0 || posicion >= this->capacidad)
+        {
+            throw std::out_of_range{ "Indice fuera de rango." };
+        }
+    }
+
+    void verificar_indice(int posicion_1, int posicion_2)
+    {
+        if ((posicion_1 < 0 || posicion_1 >= this->capacidad)
+            || (posicion_2 < 0 || posicion_2 >= this->capacidad))
+        {
+            throw std::out_of_range{ "Indice fuera de rango." };
+        }
+    }
+
+    void verificar_existencia(int indice)
+    {
+        if (this->lista_nodos[indice] == nullptr)
+        {
+            throw std::invalid_argument
+            {
+                "El nodo con indice " + std::to_string(indice) + " no existe."
+            };
+        }
     }
 };
